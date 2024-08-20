b109dd297a059c0ba1531a9e60b3189db19a4f39
ali <example@email.com>
2024-08-20 09:36:27 +0300

    changed git config

--- modules/home-manager/apps/git.nix
+++ modules/home-manager/apps/git.nix
@@ -1,23 +1,40 @@
 {pkgs, ...}: {
+  home.file = {
+    ".gitconfig".text = ''
+      [user]
+      	email = example@email.com
+      	name = ali
+      [credential]
+      	helper = store
+      [init]
+      	defaultBranch = main
+      [filter "lfs"]
+      	clean = git-lfs clean -- %f
+      	smudge = git-lfs smudge -- %f
+      	process = git-lfs filter-process
+      	required = true
+    '';
+  };
   programs.git = {
     enable = true;
-    extraConfig = {
-      init = {
-        defaultBranch = "main";
-      };
-      url = {
-        "https://github.com/" = {
-          insteadOf = [
-            "gh:"
-            "github:"
-          ];
-        };
-      };
-      credential.helper = "${
-        pkgs.git.override {withLibsecret = true;}
-      }/bin/git-credential-libsecret";
-    };
-    userName = "ali";
-    userEmail = "example@email.com";
   };
+  #   extraConfig = {
+  #     init = {
+  #       defaultBranch = "main";
+  #     };
+  #     url = {
+  #       "https://github.com/" = {
+  #         insteadOf = [
+  #           "gh:"
+  #           "github:"
+  #         ];
+  #       };
+  #     };
+  #     credential.helper = "${
+  #       pkgs.git.override {withLibsecret = true;}
+  #     }/bin/git-credential-libsecret";
+  #   };
+  #   userName = "ali";
+  #   userEmail = "example@email.com";
+  # };
 }
