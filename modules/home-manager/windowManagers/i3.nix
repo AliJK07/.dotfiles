4f7c9ddeb2a01fe90d01bac88639af693090c65a
ali <ali@mail.com>
2024-11-27 02:09:33 +0300

    disabled mouse accel and update

--- modules/home-manager/windowManagers/i3.nix
+++ modules/home-manager/windowManagers/i3.nix
@@ -40,6 +40,11 @@ in {
             notification = false;
           }
           {
+            command = "for id in $(xinput list | grep ' pointer ' | cut -d '=' -f 2 | cut -f 1); do xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1; done";
+            always = true;
+            notification = false;
+          }
+          {
             command = "steam";
             always = false;
             notification = false;
